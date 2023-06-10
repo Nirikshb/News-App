035e3e5e32ff8471284c8b3e1787e188b58ccfe0
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-06-10 16:18:22 +0530

    solution

--- src/Articles.js
+++ src/Articles.js
@@ -1,15 +1,16 @@
 import React from "react";
-
 // ArticleItem component
 export const ArticleItem = ({ article }) => {
   return (
     <div className="provider">
-       <h1>
-         <a href={article.url}>{article.title}</a>
-       </h1>
-      <p style={{ margin: "5px" }}>{article.description}</p>
-      <p>{article.publishedAt.split("T")[0]}</p>
       <img className="picture" src={article.urlToImage} alt="" />
+      <div>
+        <h1>
+          <a href={article.url}>{article.title}</a>
+        </h1>
+        <p className="description">{article.description}</p>
+        <p className="published-date">{article.publishedAt.split("T")[0]}</p>
+      </div>
     </div>
   );
 };
@@ -17,11 +18,11 @@ export const ArticleItem = ({ article }) => {
 // Articles component
 const Articles = ({ articles }) => {
   return (
-    <>
+    <div className="articles-container">
       {articles.map((article, index) => (
         <ArticleItem article={article} key={article.title + index} />
       ))}
-    </>
+    </div>
   );
 };
 
